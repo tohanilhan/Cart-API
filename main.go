630a209163e26762a7c15c808cf153bc2653cdfc
tohani <tohani@test.com>
2022-08-07 02:52:41 +0300

    given amount and user id initializers added

--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"log"
 	"os"
+	"strconv"
+	"sync"
 
+	"github.com/google/uuid"
 	"github.com/tohanilhan/Patika.dev-Property-Finder-Go-Bootcamp-Final-Project/db"
 	configs "github.com/tohanilhan/Patika.dev-Property-Finder-Go-Bootcamp-Final-Project/pkg/config"
 	"github.com/tohanilhan/Patika.dev-Property-Finder-Go-Bootcamp-Final-Project/pkg/middleware"
 	"github.com/tohanilhan/Patika.dev-Property-Finder-Go-Bootcamp-Final-Project/pkg/routes"
 	"github.com/tohanilhan/Patika.dev-Property-Finder-Go-Bootcamp-Final-Project/pkg/utils"
+	"github.com/tohanilhan/Patika.dev-Property-Finder-Go-Bootcamp-Final-Project/vars"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/joho/godotenv"
@@ -17,11 +21,19 @@ import (
 var (
 	app    *fiber.App
 	config fiber.Config
+	err    error
+	once   sync.Once
 )
 
 func init() {
 	// load env file
 	godotenv.Load()
+
+	// init user id
+	initUserID()
+
+	// init given amount
+	initGivenAmount()
 
 	// verify db connection
 	initDb()
@@ -71,4 +83,23 @@ func initFiber() {
 func initDb() {
 	db.InitDb()
 	log.Println("Db init OK.")
+}
+
+func initUserID() {
+	// UserId will be created only once when program starts by using singleton pattern
+	once.Do(func() {
+		vars.UserId = uuid.New()
+	})
+
+}
+
+func initGivenAmount() {
+
+	once.Do(func() {
+		vars.GivenAmount, err = strconv.ParseFloat(os.Getenv("GIVEN_AMOUNT"), 64)
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+
 }
